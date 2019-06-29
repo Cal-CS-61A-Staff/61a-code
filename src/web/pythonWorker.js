082882e7ef22b866d97abb58b5695f30748b1c45
rahularya <rahularya@berkeley.edu>
2019-06-29 04:30:34 -0700

    fix: removed expensive debug statement

--- src/web/pythonWorker.js
+++ src/web/pythonWorker.js
@@ -50,7 +50,7 @@ function initialize() {
 }
 
 function initializePython() {
-    console.log(__BRYTHON__.python_to_js(code)); // TODO: DISABLE!!!
+    // console.log(__BRYTHON__.python_to_js(code)); // TODO: DISABLE!!!
     __BRYTHON__.run_script(code, "__main__", true);
 }
 
